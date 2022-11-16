94fd9d08d15531b21a701cf1c0c8b630335f5406
toastythetoaster <toast@isota.ch>
2022-11-16 15:31:32 -0500

    Build for ESNext

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: 'esnext',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[hash][extname]',
